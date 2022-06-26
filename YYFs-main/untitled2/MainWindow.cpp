897b281d93ec52912794e50e5f2d1e9ae0cba492
yyf的项目 <2942740450@qq.com>
2022-06-26 22:44:50 +0800

    完成修改员工数据的功能

--- YYFs-main/untitled2/MainWindow.cpp
+++ YYFs-main/untitled2/MainWindow.cpp
@@ -76,7 +76,7 @@ void MainWindow::on_pushButton_5_clicked()  //查询员工信息
     db.open();
 
     QSqlQueryModel *model=new QSqlQueryModel;
-    QString sql=QString("select * from employee_information where %1 = %2").arg(ui->comboBox->currentText()).arg(ui->textEdit->toPlainText());
+    QString sql=QString("select * from employee_information where %1 = \"%2\"").arg(ui->comboBox->currentText()).arg(ui->textEdit->toPlainText());
     model->setQuery(sql,db);
     model->setHeaderData(0,Qt::Horizontal,tr("员工编号"));
     model->setHeaderData(1,Qt::Horizontal,tr("姓名"));
@@ -106,19 +106,27 @@ void MainWindow::on_pushButton_6_clicked()
     db.setPort(3306);
     db.open();
 
-    QString sql_2=QString("select * from employee_information where %1 = %2").arg("id").arg(ui->textEdit->toPlainText());
-    QString sql= QString("update employee_information set %1 = %2 where %3 =%4").arg(ui->comboBox_2->currentText()).arg(ui->textEdit_2->toPlainText())
+    QString sql_2=QString("select * from employee_information where %1 = %2").arg(ui->comboBox->currentText()).arg(ui->textEdit->toPlainText().toInt());
+    QString sql= QString("update employee_information set %1 = \"%2\" where %3 =\"%4\"").arg(ui->comboBox_2->currentText()).arg(ui->textEdit_2->toPlainText())
             .arg(ui->comboBox->currentText()).arg(ui->textEdit->toPlainText());
     QSqlQuery query(db);
+    query.exec(sql);
 
-   if(query.exec(sql_2))
-   {
-       qDebug()<<"chenggong";
-   }
     QSqlQueryModel *model_2=new QSqlQueryModel;
 
 
 
+    model_2->setQuery(sql_2,db);
+    model_2->setHeaderData(0,Qt::Horizontal,tr("员工编号"));
+    model_2->setHeaderData(1,Qt::Horizontal,tr("姓名"));
+    model_2->setHeaderData(2,Qt::Horizontal,tr("性别"));
+    model_2->setHeaderData(3,Qt::Horizontal,tr("出生日期"));
+    model_2->setHeaderData(4,Qt::Horizontal,tr("入职时间"));
+    model_2->setHeaderData(5,Qt::Horizontal,tr("所属部门"));
+    model_2->setHeaderData(6,Qt::Horizontal,tr("职位"));
+    model_2->setHeaderData(7,Qt::Horizontal,tr("职称"));
+    model_2->setHeaderData(8,Qt::Horizontal,tr("政治面貌"));
+    model_2->setHeaderData(9,Qt::Horizontal,tr("婚姻情况"));
 
     ui->tableView->setModel(model_2);
 
